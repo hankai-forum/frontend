9c47f4bbdfbcddd378b714461c913e45548d4193
yxz <yixuan0808@gmail.com>
2023-12-20 09:18:25 +0800

    Show frontend and backend versions

---
+++ src/components/Version.vue
@@ -0,0 +1,10 @@
+<!-- vim: set ts=2 sts=2 sw=2: -->
+<script setup>
+const response = await fetch("http://localhost:3000/api/version", { method: 'GET' }, {});
+const data = await response.json();
+</script>
+<template>
+  <p>Backend Version: {{ data.version }}</p>
+  <br>
+  <p>Frontend Version: {{ APP_VERSION }}</p>
+</template>
