0322e54674f0f00c16c51951ffe08327c8c041ab
yxz <yixuan0808@gmail.com>
2024-01-04 20:36:01 +0800

    Add GPL v3 license

--- src/components/icons/IconTooling.vue
+++ src/components/icons/IconTooling.vue
@@ -1,3 +1,21 @@
+/*
+ * A forum software for use in a school in China.
+ * Copyright (C) 2023-2024 Fustigate & YZ9551(YXZ)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 <!-- This icon is from <https://github.com/Templarian/MaterialDesign>, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
 <template>
   <svg
